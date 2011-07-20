c8f932be250018486badfd56a9cb9557f401575f
Roberto Di Cosmo <roberto@dicosmo.org>
2011-07-20 23:12:27 +0200

    cleaned up code using xen mmap

--- parmap.ml
+++ parmap.ml
@@ -14,23 +14,14 @@
 open Common
 open Util
 
-(* informations from child to parents *)
+(* timers *)
 
-type outcome = Res of payload | Fail of exn
-and payload = int * int (* pid + size of result *)
-
-(* create a pipe, and return the access functions, as well as the underlying file descriptor *)
-
-let mkpipe () =
-    let (ifd,ofd) = Unix.pipe () in
-    let (ic, oc) = (Unix.in_channel_of_descr ifd, Unix.out_channel_of_descr ofd) in
-    ((fun x ->  Marshal.to_channel oc x [Marshal.Closures]; flush oc),
-     (fun () -> Marshal.from_channel ic),
-     (fun () -> close_out oc),
-     (fun () -> close_in ic),
-     ifd
-    )
-;;  
+let t = Timer.create "collection";;
+Timer.enable "collection";;
+let tc = Timer.create "computation";;
+Timer.enable "computation";;
+let tm = Timer.create "marshalling";;
+Timer.enable "marshalling";;
 
 (* create a shadow file descriptor *)
 
@@ -42,52 +33,57 @@ let tempfd () =
     fd
   with e -> Unix.unlink name; raise e
 
-(* readers *)
-
-type 'a reader = (unit -> 'a) option
-
-(* unmarshaling from a mmap *)
+(* unmarshaling from a fd using a mmap *)
 
 let unmarshal_using_mmap fd =
+  (* read the header, to find out the size of the rest of the data *)
   let mm = Mmap.mmap fd Mmap.RDWR Mmap.SHARED Marshal.header_size 0 in 
   let header = Mmap.read mm 0 Marshal.header_size in
   Mmap.unmap mm;
   let size = Marshal.total_size header 0 in
+  (* now map the full length of the data, and do the unmarshaling *)
   Printf.eprintf "Reading %d bytes from mmap\n" size;
   let mm = Mmap.mmap fd Mmap.RDWR Mmap.SHARED size 0 in 
   let s = Mmap.read mm 0 size in
-  Printf.eprintf "Read %d bytes from mmap\n" size;  
+  Printf.eprintf "Read %d bytes from mmap\n" size;
   Mmap.unmap mm;
   Marshal.from_string s 0
 ;;
 
-let map_size = 60000000;;
+(* marshaling to a fd using a mmap *)
+
+let marshal_using_mmap pid fd v = 
+  Timer.start tm;
+  let s = Marshal.to_string v [Marshal.Closures] in
+  let sl = (String.length s) in
+  Timer.stop tm (); Timer.pp_timer Format.std_formatter tm;
+  Printf.eprintf "Process %d has marshaled result of size %d\n" pid sl;
+  (* stretch the file to the required size *)
+  ignore(Unix.lseek fd sl Unix.SEEK_SET); 
+  ignore(Unix.write fd "0" 0 1);
+  (* rewind the file, probably not needed *)
+  ignore(Unix.lseek fd 0 Unix.SEEK_SET); 
+  (* create the mmap *)
+  let mm = Mmap.mmap fd Mmap.RDWR Mmap.SHARED sl 0 in
+  (* write marshal output on the mmap *)
+  Mmap.write mm s 0 sl;
+  Printf.eprintf "Process %d has successfully written result of size %d\n" pid sl;;
 
 (* the parallel map function *)
 
 let parmap (f:'a -> 'b) (l:'a list) ?(ncores=1) : 'b list=
-  let t = Timer.create "collection" in
-  Timer.enable "collection";
-  let tc = Timer.create "computation" in
-  Timer.enable "computation";
-  let tm = Timer.create "marshalling" in
-  Timer.enable "marshalling";
   Timer.start tc;
   (* flush everything *)
   flush stdout; flush stderr;
   (* init task parameters *)
   let ln = List.length l in
   let chunksize = ln/ncores in
-  let readers = Array.init ncores (fun _ -> None) in
   (* create the file descriptors used for mmapping *)
   let fdarr=Array.init ncores (fun _ -> tempfd()) in
   for i = 0 to ncores-1 do
-    (* create a pipe for writing back the results *)
-    let (writep,readp,closew,closer,ifd) = mkpipe() in
        match Unix.fork() with
       0 -> 
 	begin
-          closer();
           let pid = Unix.getpid() in
           let reschunk=ref [] in
           let limit=if i=ncores-1 then ln-1 else (i+1)*chunksize-1 in
@@ -97,59 +93,26 @@ let parmap (f:'a -> 'b) (l:'a list) ?(ncores=1) : 'b list=
 	    with _ -> (Printf.printf "Error: j=%d\n" j)
           done;
           Printf.eprintf "Process %d done computing\n" pid; flush stderr;
-          Timer.start tm;
-          let s = Marshal.to_string (List.rev !reschunk) [Marshal.Closures] in
-          let sl = (String.length s) in
-          Timer.stop tm (); Timer.pp_timer Format.std_formatter tm;
-          Printf.eprintf "Process %d has marshaled result of size %d\n" pid sl;
-          (* stretch the file to the required size *)
-          ignore(Unix.lseek fdarr.(i) sl Unix.SEEK_SET); 
-          ignore(Unix.write fdarr.(i) "0" 0 1);
-          (* rewind the file *)
-          ignore(Unix.lseek fdarr.(i) 0 Unix.SEEK_SET); 
-          (* create the mmap *)
-          let mm = Mmap.mmap fdarr.(i) Mmap.RDWR Mmap.SHARED sl 0 in
-          (* write marshal output on the mmap *)
-	  Mmap.write mm s 0 sl;
-          Printf.eprintf "Process %d has successfully written result of size %d\n" pid sl;
-          let s' = Mmap.read mm 0 sl in
-          if s=s' then
-            Printf.eprintf "Process %d has successfully re-read and verified result of size %d\n" pid sl
-          else 
-            Printf.eprintf "Process %d has successfully re-read result of size %d\n" pid sl;
-          writep sl; 
-          closew();
+          marshal_using_mmap pid fdarr.(i) (List.rev !reschunk);
           exit 0
 	end
     | -1 ->  Printf.eprintf "Fork error: pid %d; i=%d.\n" (Unix.getpid()) i; 
-    | pid -> (* collect the worker io data *)
-	(closew();(readers.(i)<- Some readp))
+    | pid -> ()
+
   done;
   (* wait for all childrens *)
   for i = 0 to ncores-1 do try ignore(Unix.wait()) with Unix.Unix_error (Unix.ECHILD, _, _) -> () done;
+  Timer.stop tc (); Timer.pp_timer Format.std_formatter tc;
   (* read in all data *)
-  let sizes = Array.init ncores (fun _ -> 0) in
-  let maxs = ref 0 in
-  for i=0 to ncores-1 do
-    match readers.(i) 
-    with 
-      Some reader -> sizes.(i)<-reader(); maxs := max sizes.(i) !maxs
-    | _ -> failwith (Printf.sprintf "No registered reader at index %d." i)
-  done;
-  (* copy-buffer option *)
+  Timer.start t;
   let res = ref [] in
   for i = 0 to ncores-1 do
       let v = unmarshal_using_mmap fdarr.(i) in 
       res:= v::!res;
   done;
-  Timer.stop tc ();  Timer.pp_timer Format.std_formatter tc;
-  (* is _this_ taking too much time? *)
-  Timer.start t;
-  let l = 
-    List.flatten (List.rev !res)
-  in 
-  Timer.stop t (); Timer.pp_timer Format.std_formatter t;
-  l 
+  let l = List.flatten (List.rev !res) in
+  Timer.stop t (); Timer.pp_timer Format.std_formatter tc;
+  l
 ;;
 
 
