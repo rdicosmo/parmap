56677eb4a14ada56a2eaa421a54b8cddaa0bcd7f
Roberto Di Cosmo <roberto@dicosmo.org>
2011-10-26 17:20:50 +0200

    Redirect stdout/stderr to one separate file per worker

--- parmap.ml
+++ parmap.ml
@@ -13,6 +13,17 @@
 
 open ExtLib
 
+(* freopen emulation, from Xavier's suggestion on OCaml mailing list *)
+
+let reopen_out outchan filename =
+  flush outchan;
+  let fd1 = Unix.descr_of_out_channel outchan in
+  let fd2 =
+    Unix.openfile filename [Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC] 0o666 in
+  Unix.dup2 fd2 fd1;
+  Unix.close fd2
+
+
 (* unmarshal from a mmap seen as a bigarray *)
 let unmarshal fd =
  let a=Bigarray.Array1.map_file fd Bigarray.char Bigarray.c_layout true (-1) in
@@ -71,6 +82,9 @@ let parmapfold ?(ncores=1) (f:'a -> 'b) (s:'a sequence) (op:'b->'c->'c) (opid:'c
 	begin
           let pid = Unix.getpid() in
           let reschunk=ref opid in
+          (* send stdout and stderr to a file to avoid mixing output from different cores *)
+	  reopen_out stdout (Printf.sprintf "stdout.%d" i);
+	  reopen_out stderr (Printf.sprintf "stderr.%d" i);
           let lo=i*chunksize in
           let hi=if i=ncores-1 then ln-1 else (i+1)*chunksize-1 in
           (* iterate in reverse order, to accumulate in the right order *)
