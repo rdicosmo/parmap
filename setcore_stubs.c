8f6db6168a7531f012bab19f3d414f1c0789d411
Pietro Abate <pietro.abate@pps.jussieu.fr>
2011-11-17 14:26:40 +0100

    Added configure and moved to ocamlbuild.

--- setcore_stubs.c
+++ setcore_stubs.c
@@ -4,6 +4,7 @@
 #include <sched.h>
 #include <errno.h>
 #include <caml/mlvalues.h>
+#include "config.h"
 
 CAMLprim value setcore(value which) {
   int numcores = sysconf( _SC_NPROCESSORS_ONLN );
@@ -16,6 +17,7 @@ CAMLprim value setcore(value which) {
       CPU_ZERO(&cpus); 
       CPU_SET (w,&cpus);
       //fprintf(stderr,"Trying to pin to cpu %d out of %d reported by the system\n",w,numcores);
+#ifdef HAVE_DECL_SCHED_SETAFFINITY
       retcode = sched_setaffinity(getpid(), sizeof(cpu_set_t), &cpus);
       if(retcode != 0) {
 	fprintf(stderr,"Failed pinning to cpu %d, trying %d/2\n",w, w); 
@@ -25,6 +27,9 @@ CAMLprim value setcore(value which) {
 	{ //fprintf(stderr,"Succeeded pinning to cpu %d\n",w); 
 	  finished=1;
 	}
+#else
+      finished=1;
+#endif
     }
   return Val_unit;
 }
