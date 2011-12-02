737abc8538914db2bd87e97089ab0b80fe37b6c0
Marco Danelutto <marcod@di.unipi.it>
2011-12-02 22:38:39 +0100

    Initial support for setcore on Mac OS X

--- setcore_stubs.c
+++ setcore_stubs.c
@@ -1,20 +1,32 @@
+#include "config.h"
 #include <stdio.h>
 #include <unistd.h>
+#ifdef HAVE_MACH_THREAD_POLICY_H
+#include <mach/mach_init.h>
+#include <mach/thread_policy.h>
+// #include <mach/sched.h>
+#endif
+#ifdef HAVE_DECL_SCHED_SETAFFINITY
 //#define _GNU_SOURCE             /* See feature_test_macros(7) */
 #include <sched.h>
+#endif
 #include <errno.h>
 #include <caml/mlvalues.h>
-#include "config.h"
 
 CAMLprim value setcore(value which) {
-#ifdef HAVE_DECL_SCHED_SETAFFINITY
   int numcores = sysconf( _SC_NPROCESSORS_ONLN );
   int w = Int_val(which) % numcores; // stay in the space of existing cores
+#ifdef HAVE_DECL_SCHED_SETAFFINITY
   cpu_set_t cpus;   
+#endif
+#ifdef HAVE_MACH_THREAD_POLICY_H
+  thread_affinity_policy_data_t affinityData;
+#endif
   int retcode;
   int finished=0;
   while (finished==0)
     {
+#ifdef HAVE_DECL_SCHED_SETAFFINITY
       CPU_ZERO(&cpus); 
       CPU_SET (w,&cpus);
       //fprintf(stderr,"Trying to pin to cpu %d out of %d reported by the system\n",w,numcores);
@@ -23,11 +35,22 @@ CAMLprim value setcore(value which) {
 	fprintf(stderr,"Failed pinning to cpu %d, trying %d/2\n",w, w); 
 	w=w/2;
       }
+#endif
+#ifdef HAVE_MACH_THREAD_POLICY_H
+      affinityData.affinity_tag = w;
+      retcode = thread_policy_set(mach_thread_self(),
+                        THREAD_AFFINITY_POLICY,
+                        affinityData,
+                        THREAD_AFFINITY_POLICY_COUNT);
+      if(retcode) {
+        fprintf(stderr,"MAC OS X: Failed pinning to cpu %d, trying %d/2\n",w, w);
+        w=w/2;
+      }
+#endif
       else 
 	{ //fprintf(stderr,"Succeeded pinning to cpu %d\n",w); 
 	  finished=1;
 	}
     }
-#endif
   return Val_unit;
 }
