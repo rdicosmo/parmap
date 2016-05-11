970e49b10446782ec305075648115d5e03637bcb
Roberto Di Cosmo <github@dicosmo.org>
2016-05-11 17:07:32 +0200

    Reverting one option that still breaks compilation in 4.02, leaving open to fix

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -7,7 +7,7 @@ let _ = dispatch begin function
       flag ["compile"; "c"] & S[ A"-ccopt"; A"-D_GNU_SOURCE"; A"-ccopt"; A"-fPIC" ];
 
       flag ["link"; "library"; "ocaml"; "byte"; "use_libparmap"] &
-        S[A"-dllib"; A"-lparmap_stubs"; A"setcore.cmo"];
+        S[A"-dllib"; A"-lparmap_stubs"; ];
       flag ["link"; "library"; "ocaml"; "native"; "use_libparmap"] &
           S[A"-cclib"; A"-lparmap_stubs"; A"setcore.cmx"];
       dep ["link"; "ocaml"; "use_libparmap"] ["libparmap_stubs.a"];
