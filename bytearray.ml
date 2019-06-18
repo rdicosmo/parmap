48711a6123f93a3a2e869ae49ee8c9cbf17cd840
Christopher Zimmermann <madroach@gmerlin.de>
2019-06-18 22:36:53 +0200

    add compatibility layer for Unix/Genarray.map_file

--- bytearray.ml
+++ bytearray.ml
@@ -61,7 +61,9 @@ let of_string s =
 
 let mmap_of_string fd s =
   let l = String.length s in
-  let ba = Bigarray.Array1.map_file fd Bigarray.char Bigarray.c_layout true l in
+  let ba =
+    Bigarray.array1_of_genarray @@
+    Parmap_compat.map_file fd Bigarray.char Bigarray.c_layout true [|l|] in
   unsafe_blit_from_string s 0 ba 0 l;
   ba
 (*
